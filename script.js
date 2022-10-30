4380d62871bddb4d7a565ee747f76ddd5e183d3c
ralph Samonte pabua <87641822+pabuaralph55@users.noreply.github.com>
2022-10-30 15:43:12 +0800

    script modified

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 window.onload = function () {
 
-        let seconds = 00;
-        let tens = 00;
-        let minutes = 00;
-        let hours = 00;
-        let appendTens = document.getElementById("tens");
-        let appendSeconds = document.getElementById("seconds");
-        let appendMinutes = document.getElementById("minutes");
-        let appendHours = document.getElementById("hours");
-        let buttonStart = document.getElementById("button-start");
-        let buttonStop = document.getElementById("button-stop");
-        let buttonReset = document.getElementById("button-reset");
-        let Interval;
+        let seconds = 00,
+        tens = 00,
+        minutes = 00,
+        hours = 00,
+        appendTens = document.getElementById("tens"),
+        appendSeconds = document.getElementById("seconds"),
+        appendMinutes = document.getElementById("minutes"),
+        appendHours = document.getElementById("hours"),
+        buttonStart = document.getElementById("button-start"),
+        buttonStop = document.getElementById("button-stop"),
+        buttonReset = document.getElementById("button-reset"),
+        Interval;
 
         buttonStart.onclick = function() {
             clearInterval(Interval);
